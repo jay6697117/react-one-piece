1136f8f650c00cdacaf4a6a21ff464d37dbc792d
steve zhang <871506263@qq.com>
2022-10-13 12:03:56 +0800

    提交

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,19 @@ import './assets/style/App.css';
 
 function App() {
   const obj = { name: '奶茶', age: 20 };
+  const users = ['章三', '李四', '王五'];
   return (
     <div className='App'>
       <Parent>
         <Child {...obj} />
       </Parent>
+      <div style={{ padding: '10px', backgroundColor: '#ccc' }}>
+        {users.map((item, index) => (
+          <div style={{ margin: '10px 0', backgroundColor: 'green' }} key={index}>
+            {item}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
